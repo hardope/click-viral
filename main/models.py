67702e7b8b907c0b35e39b34d9ff9a841b3c54d9
Jeremiah Pythonanywhere <adeyeriopeoluwa05@gmail.com>
2023-04-19 17:18:52 +0000

    Updated

---
+++ main/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+import uuid
+from datetime import datetime
+from django.contrib.auth.models import User
+from main.parse_time import get_time
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.CharField(max_length=1000, default="")
+    image = models.CharField(max_length=50, default="empty")
+    gender = models.CharField(max_length=20, default="Null")
+    visibility = models.CharField(max_length=20, default="visible")
+    followers = models.IntegerField(default=0)
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    article = models.TextField(default="")
+    media = models.CharField(max_length=100, default="empty")
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    edited = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=datetime.now)
+    edited_at = models.DateTimeField(default=datetime.now)
+
+
+    def to_dict(self, user):
+        try:
+            count_likes = Like.objects.get(user_id=user, post_id=self.id)
+            like_value = "True"
+        except:
+            like_value = "False"
+
+        return {"id": str(self.id),
+                "name": self.user,
+                "article": self.article,
+                "media": self.media,
+                "likes": self.likes,
+                "like_value": like_value,
+                "created_at": f"{get_time(self.created_at)}",
+                "comments": self.comments,
+                "edited": self.edited,
+                "edited_at": f"{get_time(self.created_at)}",
+                }
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, null=True)
+    m_comment = models.ForeignKey('self', on_delete = models.CASCADE, null=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    article = models.TextField(default="")
+    media = models.CharField(max_length=100, default="empty")
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    edited = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=datetime.now)
+    edited_at = models.DateTimeField(default=datetime.now)
+
+    def to_dict(self, user):
+        try:
+            try:
+                count_likes = Like.objects.get(user_id=user, post_id=self.id)
+            except:
+                count_likes = Like.objects.get(user_id=user, comment_id=self.id)
+            like_value = "True"
+        except:
+            like_value = "False"
+
+        return {"id": str(self.id),
+                "post": str(self.post_id),
+                "name": self.user,
+                "article": self.article,
+                "media": self.media,
+                "likes": self.likes,
+                "like_value": like_value,
+                "created_at": f"{get_time(self.created_at)}",
+                "comments": self.comments,
+                "edited": self.edited,
+                "edited_at": f"{get_time(self.created_at)}",
+                }
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, null=True)
+    comment = models.ForeignKey(Comment, on_delete = models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    time = models.DateTimeField(default=datetime.now)
+
+    def name_user(self):
+        return User.objects.get(id=self.user_id).username
+
+
+
+
+
+
+
---
+++ main/views.py
@@ -0,0 +1,304 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Post, Like, Comment
+import json
+import sys
+import os
+from datetime import datetime, timezone
+
+root = "/home/clickviral/viral"
+# Create your views here.
+
+def feed(request):
+
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+    user= request.user.id
+    try:
+        posts = Post.objects.all()
+        posts = [i.to_dict(user) for i in posts]
+        posts.reverse()
+
+    except:
+        posts = []
+
+    return render(request, "posts.html", {"username": request.user.username, "posts": posts})
+
+def comment(request, query):
+
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+    if request.method == "POST":
+        comment_article = request.POST.get("comment")
+        media = "empty"
+        user_id = request.user.id
+        try:
+            media_file = request.FILES['media']
+            media = str(media_file).split('.')[1]
+
+
+            assert media_file is not None
+            try:
+                update = Post.objects.get(id=query)
+                update.comments+=1
+                update.save()
+
+                new_comment = Comment(media=media, user_id=user_id, article=comment_article, post_id=query)
+            except:
+                update = Comment.objects.get(id=query)
+                update.comments+=1
+                update.save()
+
+                new_comment = Comment(media=media, user_id=user_id, article=comment_article, m_comment_id=query)
+
+            with open(f"{root}/media/posts/{new_comment.id}.{media}", 'wb+') as file:
+                for chunk in media_file.chunks():
+                    file.write(chunk)
+
+        except:
+
+            try:
+                update = Post.objects.get(id=query)
+                update.comments+=1
+                update.save()
+
+                new_comment = Comment(media=media, user_id=user_id, article=comment_article, post_id=query)
+            except:
+                update = Comment.objects.get(id=query)
+                update.comments+=1
+                update.save()
+
+                new_comment = Comment(media=media, user_id=user_id, article=comment_article, m_comment_id=query)
+
+        new_comment.save()
+
+        return HttpResponseRedirect(f"{query}")
+
+    else:
+        try:
+            post = Post.objects.get(id=query).to_dict(request.user.id)
+        except:
+            post = Comment.objects.get(id=query).to_dict(request.user.id)
+
+        comments = Comment.objects.filter(post_id=query)
+        comments1 = Comment.objects.filter(m_comment_id=query)
+
+        comments = [i.to_dict(request.user.id) for i in comments] + [i.to_dict(request.user.id) for i in comments1]
+
+        return render(request, "comment.html", {"post":post, "comments":comments})
+
+
+def edit_post(request, query):
+
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+    if request.method == "POST":
+        article = request.POST.get("post").strip()
+
+        try:
+            post = Post.objects.get(id=query)
+        except:
+            post = Comment.objects.get(id=query)
+
+        post.article = article
+        post.edited_at = datetime.now()
+        post.edited = True
+        post.save()
+        pid = query
+
+        return redirect(f"/comment/{pid}")
+
+    else:
+        pid = query
+        editable = True
+        try:
+            post = Post.objects.get(id=query)
+        except:
+            post = Comment.objects.get(id=query)
+        created = post.created_at
+        now = datetime.now(timezone.utc)
+        diff = now - created
+        post = post.to_dict(request.user.id)
+        if post['name'] != request.user:
+            return redirect("/comment/{pid}")
+        if diff.total_seconds() > 1800:
+            editable = False
+
+        return render(request, "edit_post.html", {"post": post, "editable": editable})
+
+def profile(request, query):
+    try:
+        user = User.objects.get(username=query)
+    except:
+        return render(request, "nopage.html")
+
+    return render(request, "profile.html", {"user": user})
+
+def delete(request, query):
+    try:
+        post = Post.objects.get(id=query)
+    except:
+        post = Comment.objects.get(id=query)
+    if post.media != "empty":
+        os.remove(f"/home/clickviral/viral/media/posts/{post.id}.{post.media}")
+    else:
+        pass
+    try:
+        post = Post.objects.get(id=query).delete()
+    except:
+        post = Comment.objects.get(id=query).delete()
+    return redirect("/")
+
+def fetch_posts(request):
+     posts = Post.objects.all()
+     posts = [i.to_dict() for i in posts]
+
+     return JsonResponse(posts, safe=False)
+
+def view_likes(request, query):
+    try:
+        try:
+            list_likes = Like.objects.all().filter(post_id=query)
+        except:
+            list_likes = Like.objects.all().filter(comment_id=query)
+        names = [i.name_user() for i in list_likes]
+    except:
+        names = []
+
+    return HttpResponse(json.dumps(names))
+
+def notification(request):
+
+    return HttpResponse(json.dumps([]))
+
+def like(request, query):
+    id = request.user.id
+    try:
+        try:
+            Like.objects.get(user_id=id, post_id=query)
+        except:
+            Like.objects.get(user_id=id, comment_id=query)
+    except:
+        try:
+            update_post = Post.objects.get(id=query)
+            update_post.likes+=1
+            update_post.save()
+
+            new_like = Like(post_id=query, user_id=id)
+        except:
+            update_post = Comment.objects.get(id=query)
+            update_post.likes+=1
+            update_post.save()
+
+            new_like = Like(comment_id=query, user_id=id)
+
+        new_like.save()
+
+    return HttpResponse(json.dumps([]))
+
+def unlike(request, query):
+    id = request.user.id
+
+    try:
+        update_post = Post.objects.get(id=query)
+        update_post.likes-=1
+        update_post.save()
+        Like.objects.get(user_id=id, post_id=query).delete()
+    except:
+        update_post = Comment.objects.get(id=query)
+        update_post.likes-=1
+        update_post.save()
+        Like.objects.get(user_id=id, comment_id=query).delete()
+
+    return HttpResponse("")
+
+
+def new_post(request):
+    if request.method == "POST":
+        post_article = request.POST.get("post")
+        media = "empty"
+        user_id = request.user.id
+        try:
+           media_file = request.FILES['media']
+           media = str(media_file).split('.')[1]
+
+
+           assert media_file is not None
+
+           post = Post(media=media, user_id=user_id, article=post_article)
+
+           with open(f"{root}/media/posts/{post.id}.{media}", 'wb+') as file:
+                for chunk in media_file.chunks():
+                    file.write(chunk)
+
+        except:
+
+            post = Post(media=media, user_id=user_id, article=post_article)
+
+        post.save()
+
+        return HttpResponseRedirect(reverse("feed"))
+    return render(request, "newpost.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+           login(request, user)
+
+           return HttpResponseRedirect(reverse("feed"))
+
+        return render(request, "login.html", {
+           "message": "Invalid Credentials"
+        })
+
+    return render(request, "login.html")
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get("username").strip()
+        password = request.POST.get("password")
+        confirm = request.POST.get("confirm")
+
+        if confirm != password:
+            return render(request, "register.html", {
+                "message": "Invalid Credentials"
+            })
+
+        if confirm != password:
+            return render(request, "register.html", {
+                "message": "Invalid Credentials"
+            })
+
+        if " " in username:
+            return render(request, "register.html", {
+                "message": "Username cannot contain spaces"
+            })
+
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            login(request, user)
+            return HttpResponseRedirect(reverse("feed"))
+
+        except:
+            return render(request, "register.html", {
+                "message": "Username Taken"
+            })
+
+    return render(request, "register.html")
+
+def logout_view(request):
+
+    logout(request)
+
+    return HttpResponseRedirect(reverse("login"))

11de3d7cffed78b4b216ee423aa0f64e5bfa415e
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-22 12:46:18 +0000

    Updated Follow

--- main/views.py
+++ main/views.py
@@ -151,14 +151,15 @@ def edit_post(request, query):
 def profile(request, query):
     try:
         user = User.objects.get(username=query)
-        user.followers = Follow.objects.filter(user=user).count()
+        follow_count = Follow.objects.filter(user=user).count()
+        user.followers = follow_count
         user.save()
         user = User.objects.get(username=query)
         follow_value = request.user in [i.follow for i in Follow.objects.filter(user=user)]
     except:
         return render(request, "nopage.html")
 
-    return render(request, "profile.html", {"user": user, "follow_value": follow_value})
+    return render(request, "profile.html", {"user": user, "follow_value": follow_value, "f_count": follow_count})
 
 
 def delete(request, query):

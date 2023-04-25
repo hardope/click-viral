718b28c032d41dbf03dbff7a34b64ca39b436f0d
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-25 12:19:39 +0000

    Updated

--- main/models.py
+++ main/models.py
@@ -57,8 +57,7 @@ class Post(models.Model):
 
         return {
             "id": str(self.id),
-            "username": self.user,
-            "name": self.user.first_name + " " + self.user.last_name,
+            "name": self.user,
             "article": self.article,
             "media": self.media,
             "likes": count_like(self.id),

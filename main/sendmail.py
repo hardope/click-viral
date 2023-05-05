5047cdafad9f79a649178f922108ea8374f4d020
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-05 21:04:19 +0000

    Updated

---
+++ main/sendmail.py
@@ -0,0 +1,23 @@
+import os
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+
+def send_mail(to, subject, body):
+    sender = "clickviralng@gmail.com"
+    recipient = to
+    password = "hjlcnodeitfseaci"
+
+    mail = EmailMessage()
+    mail["From"] = sender
+    mail["To"] = recipient
+    mail["Subject"] = subject
+
+    mail.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(sender, password)
+        smtp.sendmail(sender, recipient, mail.as_string())

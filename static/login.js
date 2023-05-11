2851953e6fc456b50166c7953f6ceaa1d7ee28b7
Jeremiah Pythonanywhere <adeyeriopeoluwa05@gmail.com>
2023-05-11 11:28:33 +0000

    Updated

--- static/login.js
+++ static/login.js
@@ -3,40 +3,45 @@ function validateEmail(email) {
     return re.test(email);
 }
 
-function load_login(){
+function load_login() {
     let username = $("#username").val();
     let password = $("#password").val();
     let email = $("#email").val();
-    let details_message = $("#details_message")
+    let details_message = $("#details_message");
 
     if (username == "" && email == ""){
         details_message.text("Please enter a username or Email address");
-    }else if (validateEmail(email) == false){
-        details_message.innerHTML = "Invalid Email"
+    } else if (validateEmail(email) == false) {
+        details_message.text("Invalid Email");
     } else if (password == ""){
         details_message.text("Please enter a password");
     }
-    
+
     details_message.text("Logging You In...");
     
-    let url = window.location.origin
-    let request = new XMLHttpRequest();
-    var formdata = new FormData()
-    formdata.append("username", username)
-    formdata.append("email", email)
-    formdata.append("password", password)
-    request.open("POST", url + "/login")
-    request.setRequestHeader("X-CSRFToken", csrftoken);
-    request.send(formdata)
-    request.onload = () => {
-        if (request.responseText == "0"){
-            window.location.replace(url)
+    let url = window.location.origin;
+    let formdata = new FormData();
+    formdata.append("username", username);
+    formdata.append("email", email);
+    formdata.append("password", password);
+
+    $.ajax({
+        type: "POST",
+        url: url + "/login",
+        data: formdata,
+        processData: false,
+        contentType: false,
+        headers: {
+            "X-CSRFToken": csrftoken
+        },
+        success: function(response) {
+            if (response == "0"){
+                window.location.replace(url);
+            } else if (response == "1"){
+                details_message.text("Invalid Username or Password");
+            } else if (response == "2"){
+                details_message.text("Invalid");
+            }
         }
-        else if (request.responseText == "1"){
-            details_message.text("Invalid Username or Password")
-        }
-        else if (request.responseText == "2"){
-            details_message.text("Invalid")
-        }
-    }
+    });
 }

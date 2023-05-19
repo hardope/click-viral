dca6bf879d0f3e7f4cc6c453d8fdad392043ffb7
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-19 16:21:44 +0000

    Updated Views.py Format

--- main/views.py
+++ main/views.py
@@ -295,7 +295,7 @@ def get_post(request, query):
     diff = now - created
     post = Post.objects.get(id=query)
     post = post.to_dict(request.user.id)
-    if post.name != request.user:
+    if post['name'] != request.user:
         return HttpResponse("Permission Denied", status=403)
     if diff.total_seconds() > 1800:
         editable = False

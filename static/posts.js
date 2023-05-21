bfa8b53b65165233e192a2965661a3a71c413d66
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-21 16:37:05 +0000

    Jquery

--- static/posts.js
+++ static/posts.js
@@ -4,7 +4,7 @@ let c_blocks = []
 function close_comment() {
     last = c_blocks[-1]
     $('#comment_block_'+ last).remove()
-    c_blocks.pop()
+    c_blocks.splice(-1)
     if (c_blocks.length > 0){
         $('#comment_block_' + c_blocks[-1]).show();
     } else {

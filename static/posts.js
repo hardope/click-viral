0acda21146df891cae78709b34c4113e2ac8d9c0
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-26 16:22:36 +0000

    Jquery

--- static/posts.js
+++ static/posts.js
@@ -44,7 +44,6 @@ function submit_comment (id){
 }
 
 function view_comment(id){
-    console.log("Here")
     $('#main').hide();
     $('#comment_block').show();
     if (c_blocks.length > 0){
@@ -61,7 +60,6 @@ function view_comment(id){
         processData: false,
         contentType: false,
         success: function(data) {
-            console.log(data)
             // handle successful response
             var post = data.post;
             var nameContainer = '<div class="name_container"><a class="a" href="/profile/' + post.name + '" style="display: inline-flex"><img src="/static/favicon.ico" style="width: 50px; border-radius: 25px; margin-top: 20px; margin-left: 0px"><div style="margin-left: 10px; margin-top: 30px;">' + post.name + '</div></a><div style="margin-top: -10px; margin-left: 30px; font-size 1px !important;">' + post.created_at;
@@ -93,6 +91,45 @@ function view_comment(id){
 
             var upload_comment = '<div id="new_comment_form_' + post.id + '"><center><h1>New Comment</h1><center><b id="message"></b></center><textarea id="comment_article_' + post.id + '" maxlength="1000" style="width:500px; padding: 10px; height: 200px; border-radius: 10px;"></textarea><input type="file" id="comment_media_' + post.id + '" name="media" accept="image/*,video/mp4" value="" hidden><div id="label_cont"><label for="media" id="media_label">Upload Media &#128206;</label></div><button onclick=submit_comment("' + post.id + '") id="button" data-mdb-ripple-color="dark" style="font-size: 60px; color: black; border: 0ch; margin-bottom: 2%; border-radius: 10px 10px 10px 10px;  width: 200px; height: 50px;">Comment</button></center></div>'
             $('#comment_block_' + id).append(upload_comment)
+
+            for (var i = 0; i < data.comments.length; i++) {
+                var post = data.comments[i];
+                var nameContainer = '<div class="name_container"><a class="a" href="/profile/' + post.name + '" style="display: inline-flex"><img src="/static/favicon.ico" style="width: 50px; border-radius: 25px; margin-top: 20px; margin-left: 0px"><div style="margin-left: 10px; margin-top: 30px;">' + post.name + '</div></a><div style="margin-top: -10px; margin-left: 30px; font-size 1px !important;">' + post.created_at;
+                if (post.name == username) {
+                    nameContainer += '<button ' + 'onclick=edit_post("' + post.id + '")' + ' style="background-color: lightblue;border: none; margin-left: 90%; width: 40px; height:40px; border-radius: 20px; margin-bottom: 10px;" type="submit">✏️</button>';
+                } else if (post.edited == true) {
+                    nameContainer += '<button style="background-color: lightblue;border: none; margin-left: 80%; width: 100px; height:40px; border-radius: 15px; margin-bottom: 10px;" type="submit">Edited</button>';
+                }
+                nameContainer += '</div></div>';
+
+                var article = '<div class="div" id="article_' + post.id + '"><div>';
+                for (var j = 0; j < post.article.length; j++) {
+                    var element = post.article[j];
+                    article += '<' + element.tag + '>' + element.text + '</' + element.tag + '>';
+                }
+                article += '</div></div>';
+
+                var media = '';
+                if (post.media != "empty") {
+                    if (post.media == "mp4") {
+                        media += '<video src="/media/posts/' + post.id + '.mp4" controls loop preload="auto"></video>';
+                    } else {
+                        media += '<a href="/media/posts/' + post.id + '.' + post.media + '"><img src="/media/posts/' + post.id + '.' + post.media + '"></a>';
+                    }
+                }
+
+                var container = '<div class="container">';
+                if (post.like_value == "True") {
+                    container += '<p class="react" value="' + post.like_value + '" id="' + post.id + '" onclick="like(\'' + post.id + '\')">' + post.likes + ' ❤️</p>';
+                } else {
+                    container += '<p class="react" value="' + post.like_value + '" id="' + post.id + '" onclick="like(\'' + post.id + '\')">' + post.likes + ' 🖤</p>';
+                }
+                container += '<p class="comment" onclick=view_comment("'+ post.id  + '")' + '>' + post.comments + ' 💬</p><p class="v_like" onclick="view_likes(\'' + post.id + '\')">📊</p></div>';
+
+                var postElement = '<div id="' + 'post_' + post.id + '">' + (nameContainer + article + media + container) + '</div>'
+
+                $('#comment_block_' + id).append(postElement)
+            }
         },
         error: function(xhr, status, error) {
             console.log(error); // handle error response

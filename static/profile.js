82f5a817fa62b68bef0ba9295535aea91e233055
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-29 08:40:03 +0000

    Style

--- static/profile.js
+++ static/profile.js
@@ -106,7 +106,7 @@ function save_birthday(){
     }
 
     var birthdate = new Date($("#profile_birthday").val());
-    birthdate = birthdate.split(" ");
+    birthdate = birthdate.splice(" ");
     var day = birthdate[1];
     var month = birthdate[2];
     var year = birthdate[3];

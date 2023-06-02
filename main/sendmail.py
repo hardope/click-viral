9c42a51af819606dff734bb913590c2483d291f8
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-06-02 08:13:26 +0000

    Updated Views.py Format

--- main/sendmail.py
+++ main/sendmail.py
@@ -4,10 +4,10 @@ import ssl
 import smtplib
 
 
-def send_mail(to, subject, body):
-    sender = "noreply@clickviral.tech"
+def send_mails(to, subject, body):
+    sender = "clickviralng@gmail.com"
     recipient = to
-    password = "Adeopeclickviral.tech"
+    password = "wflwidljzrykjswq"
 
     mail = EmailMessage()
     mail["From"] = sender
@@ -18,6 +18,43 @@ def send_mail(to, subject, body):
 
     context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL("webmail.clickviral.tech", 465, context=context) as smtp:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
         smtp.login(sender, password)
         smtp.sendmail(sender, recipient, mail.as_string())
+
+def send_mail(to, subject, body):
+    # Set the necessary information
+    smtp_server = 'http://webmail.clickviral.tech/'
+    smtp_port = 587  # Default SMTP port for TLS
+    username = 'noreply@clickviral.tech'
+    password = 'Adeope0605'
+
+    # Set up the connection to the SMTP server
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(username, password)
+    except smtplib.SMTPException as e:
+        print(f'Error: Could not connect to the SMTP server. {str(e)}')
+        exit()
+
+    # Compose the email message
+    sender = username
+    recipient = to
+    message = body
+
+    email_body = f'''From: {sender}
+    To: {recipient}
+    Subject: {subject}
+
+    {message}'''
+
+    # Send the email
+    try:
+        server.sendmail(sender, recipient, email_body)
+        print('Email sent successfully!')
+    except smtplib.SMTPException as e:
+        print(f'Error: Could not send the email. {str(e)}')
+
+    # Close the connection to the SMTP server
+    server.quit()

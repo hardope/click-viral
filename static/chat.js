c6029e87b3ead960d6f890d9e199071945ca81d8
jeremiah-nobody <116163215+jeremiah-nobody@users.noreply.github.com>
2023-06-04 19:47:16 +0000

    UFixing DUplication

--- static/chat.js
+++ static/chat.js
@@ -37,21 +37,23 @@ $(document).ready(function(){
                 request.onload = () => {
                     if (request.status === 200) {
                         let body = $('#tab_' + user + ' #body');
-                        let existingMessages = body.find('.from-me, .from-them'); // Select existing messages
-
+                        var fid = 0;
                         for (let obj of JSON.parse(request.response)) {
-                            if (existingMessages.filter(function () { return $(this).text() === obj.message; }).length === 0) {
-                                if (obj.sender === username) {
-                                    var tag = `<p class="from-me margin-b_none" style="font-size: 20px;">${obj.message}</p>`;
-                                    body.append(tag);
-                                    var date = `<small class="from-me margin-b_none" style="text-align: right; font-size: 15px !important">${obj.created_at}</small>`;
-                                    body.append(date);
-                                } else {
-                                    var tag = `<p class="from-them" style="font-size: 20px;">${obj.message}</p>`;
-                                    body.append(tag);
-                                    var date = `<small class="from-them" style="font-size: 15px !important">${obj.created_at}</small>`;
-                                    body.append(date);
-                                }
+                            chat_counts[user] += 1;
+                            if (fid == obj.id) {
+                                continue;
+                            }
+                            fid = obj.id;
+                            if (obj.sender === username ) {
+                                var tag = `<p class="from-me margin-b_none" style="font-size: 20px;">${obj.message}</p>`
+                                body.append(tag);
+                                var date = `<small class="from-me margin-b_none" style="text-align: right; font-size: 15px !important">${obj.created_at}</small>`
+                                body.append(date)
+                            } else {
+                                var tag = `<p class="from-them" style="font-size: 20px;">${obj.message}</p>`
+                                body.append(tag);
+                                var date = `<small class="from-them" style="font-size: 15px !important">${obj.created_at}</small>`
+                                body.append(date)
                             }
                             window.scrollTo(0, 10000);
                         }
@@ -97,8 +99,8 @@ function close_chat(){
     $('#list').show();
 }
 
-function load_chat(user) {
-    let url = window.location.origin;
+function load_chat(user){
+    let url = window.location.origin
 
     let request = new XMLHttpRequest();
     request.open("GET", `${url}/get_messages/${user}-0`);
@@ -107,24 +109,20 @@ function load_chat(user) {
         chat_counts[user] = JSON.parse(request.response).length;
         if (request.status === 200) {
             let body = $('#tab_' + user + ' #body');
-            let existingMessages = body.find('.from-me, .from-them'); // Select existing messages
-
             for (let obj of JSON.parse(request.response)) {
-                if (existingMessages.filter(function () { return $(this).text() === obj.message; }).length === 0) {
-                    if (obj.sender === username) {
-                        var tag = `<p class="from-me margin-b_none" style="font-size: 20px;">${obj.message}</p>`;
-                        body.append(tag);
-                        var date = `<small class="from-me margin-b_none" style="text-align: right; font-size: 15px !important">${obj.created_at}</small>`;
-                        body.append(date);
-                    } else {
-                        var tag = `<p class="from-them" style="font-size: 20px;">${obj.message}</p>`;
-                        body.append(tag);
-                        var date = `<small class="from-them" style="font-size: 15px !important">${obj.created_at}</small>`;
-                        body.append(date);
-                    }
+                if (obj.sender === username ) {
+                    var tag = `<p class="from-me margin-b_none" style="font-size: 20px;">${obj.message}</p>`
+                    body.append(tag);
+                    var date = `<small class="from-me margin-b_none" style="text-align: right; font-size: 15px !important">${obj.created_at}</small>`
+                    body.append(date)
+                } else {
+                    var tag = `<p class="from-them" style="font-size: 20px;">${obj.message}</p>`
+                    body.append(tag);
+                    var date = `<small class="from-them" style="font-size: 15px !important">${obj.created_at}</small>`
+                    body.append(date)
                 }
-                window.scrollTo(0, 10000);
             }
+            window.scrollTo(0, 10000);
         }
     }
 }

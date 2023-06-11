82524a9dd69062795951f5f3270d357e5245a9ea
jeremiah-nobody <116163215+jeremiah-nobody@users.noreply.github.com>
2023-06-11 11:07:02 +0000

    Search

--- static/search.js
+++ static/search.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         var value = $(this).val();
         if (value.length > 0) {
             formData = new FormData();
-            FormData.append('search', value);
+            formData.append('search', value);
             $.ajax({
                 url: `${window.location.origin}/search`,
                 type: 'GET',

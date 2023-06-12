37d7fc4de38653e60ff3c2eb4f0e3a69856c350e
jeremiah-nobody <116163215+jeremiah-nobody@users.noreply.github.com>
2023-06-12 09:35:16 +0000

    Fixing Duplication

--- static/search.js
+++ static/search.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
                     if (a < 1) {
                         console.log('No matches found');
                         let no = '<p style="text-align: center; margin-top: 100px><b>No Match</b></p>'
-                        $('#search_result').append(no);
+                        $('#search_result').append(no)
                     }
                 }
             });

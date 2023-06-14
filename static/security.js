a1a9a1498e6c46a7c144b08a3cbeb7c834db03e6
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-06-14 08:12:00 +0000

    Security

--- static/security.js
+++ static/security.js
@@ -23,7 +23,6 @@ function verify_user(){
             },
             success: function(data) {
                 data = JSON.parse(data);
-                console.log(data.success)
                 if (data['success']){
                     $('#verify').hide();
                     $('#details').show();
